5540461ab7b2a1045ae1283b59f5c76adc5722fc
Filippo Pizzicola <fpizzicol@gmail.com>
2020-10-27 14:48:01 -0300

    Add option to choose the week start day

--- lib/src/clean_calendar_controller.dart
+++ lib/src/clean_calendar_controller.dart
@@ -1,10 +1,16 @@
 import 'package:intl/intl.dart';
 
 class CleanCalendarController {
+  final startWeekDay;
+
+  CleanCalendarController({this.startWeekDay = DateTime.sunday})
+      : assert(startWeekDay <= DateTime.sunday),
+        assert(startWeekDay >= DateTime.monday);
+
   List<String> getDaysOfWeek([String locale]) {
     var today = DateTime.now();
 
-    while (today.weekday != DateTime.sunday) {
+    while (today.weekday != startWeekDay) {
       today = today.subtract(Duration(days: 1));
     }
     final dateFormat = DateFormat(DateFormat.ABBR_WEEKDAY, locale);
